e52a75cfd1633e02764d671414147fb3f3f97a92
zhentosmak <zhentosmak@mail.ru>
2021-08-22 13:54:21 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const reg = /\d/g
+    const numStr = `${n}`.match(reg)
+      let reverseNum = ''
+    for (let i = numStr.length-1; i >= 0; i--) {
+      reverseNum = reverseNum + numStr[i]
+    }
+    return +reverseNum
 }
